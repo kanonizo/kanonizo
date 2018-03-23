9babd2a2c1d8814ff04242bb7659e0176361bb56
David Paterson <dpaterson1@sheffield.ac.uk>
2018-03-23 17:24:41 +0000

    adding premain class to instrument code before running test cases. Checks if classes are test classes before instrumenting

--- client/src/main/java/org/kanonizo/Premain.java
+++ client/src/main/java/org/kanonizo/Premain.java
@@ -1,17 +1,21 @@
 package org.kanonizo;
+
+import com.scythe.instrumenter.InstrumentationProperties;
 import com.scythe.instrumenter.instrumentation.ClassReplacementTransformer;
+import com.scythe.instrumenter.instrumentation.ClassReplacementTransformer.ShouldInstrumentChecker;
 import com.scythe.instrumenter.instrumentation.InstrumentingClassLoader;
+import com.scythe.instrumenter.instrumentation.MockClassLoader;
+import com.scythe.util.ClassNameUtils;
 import java.io.IOException;
-import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
-import java.security.ProtectionDomain;
+import java.net.URLClassLoader;
+import org.kanonizo.util.Util;
 
 public class Premain {
 
   private static final String[] forbiddenPackages = new String[]{"org/kanonizo", "org/junit",
       "org/apache/commons/cli", "junit", "org/apache/bcel", "org/apache/logging/log4j",
-      "org/objectweb/asm", "javax/", "java", "org/xml", "org/hamcrest"};
+      "org/objectweb/asm", "javax/", "java", "org/xml", "org/hamcrest", "com/intellij", "org/groovy"};
 
   /**
    * Premain that will be triggered when application runs with this
@@ -21,12 +25,43 @@ public class Premain {
    * @param instr Instrumentation instance to attach a ClassFileTransformer
    */
   public static void premain(String arg, Instrumentation instr) {
+    ClassReplacementTransformer.addShouldInstrumentChecker(new ShouldInstrumentChecker() {
+      private ClassReplacementTransformer crt = new ClassReplacementTransformer();
+      private MockClassLoader loader = new MockClassLoader(
+          ((URLClassLoader) ClassLoader.getSystemClassLoader()).getURLs(),
+          crt);
+
+      private boolean isTestClass(Class<?> cl) {
+
+        if (cl.isMemberClass() && isTestClass(cl.getEnclosingClass())) {
+          return true;
+        }
+        if (cl.isAnonymousClass() && isTestClass(cl.getEnclosingClass())) {
+          return true;
+        }
+        return Util.isTestClass(cl);
+      }
+
+      @Override
+      public boolean shouldInstrument(String className) {
+        try {
+          Class<?> cl = loader.loadOriginalClass(ClassNameUtils.replaceSlashes(className));
+          return !isTestClass(cl);
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        return true;
+      }
+    });
 
     for (String s : forbiddenPackages) {
       ClassReplacementTransformer.addForbiddenPackage(s);
     }
-    InstrumentingClassLoader loader = InstrumentingClassLoader.getInstance();
 
+    InstrumentingClassLoader loader = InstrumentingClassLoader.getInstance();
+    InstrumentationProperties.INSTRUMENT_BRANCHES = false;
     instr.addTransformer((l, n, c, p, buf) -> {
       try {
         return loader.modifyBytes(n, buf);
