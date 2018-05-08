7e0969254b4546cc6d6e1a2ee8e9e0dc949adfb4
David Paterson <dpaterson1@sheffield.ac.uk>
2018-05-08 08:59:21 -0700

    allowing specification of random seed using -Drandom_seed

--- client/src/main/java/org/kanonizo/util/RandomInstance.java
+++ client/src/main/java/org/kanonizo/util/RandomInstance.java
@@ -1,11 +1,18 @@
 package org.kanonizo.util;
 
+import com.scythe.instrumenter.InstrumentationProperties.Parameter;
 import java.util.Random;
 
 public class RandomInstance {
+  @Parameter(key="random_seed", description = "Some algorithms rely on making random choices to help explore unpredictable behaviour. However, to test functionality, we can specify a random seed for consistency", category="Randomness")
+  public static long RANDOM_SEED=-1;
+
   private static Random random = new Random();
 
   public static void setRandom(Random random) {
+    if(RANDOM_SEED != -1){
+      random.setSeed(RANDOM_SEED);
+    }
     RandomInstance.random = random;
   }
 
@@ -14,18 +21,30 @@ public class RandomInstance {
   }
 
   public static int nextInt() {
+    if(RANDOM_SEED != -1){
+      random.setSeed(RANDOM_SEED);
+    }
     return random.nextInt();
   }
 
   public static int nextInt(int bound) {
+    if(RANDOM_SEED != -1){
+      random.setSeed(RANDOM_SEED);
+    }
     return random.nextInt(bound);
   }
 
   public static double nextDouble() {
+    if(RANDOM_SEED != -1){
+      random.setSeed(RANDOM_SEED);
+    }
     return random.nextDouble();
   }
 
   public static boolean nextBoolean() {
+    if(RANDOM_SEED != -1){
+      random.setSeed(RANDOM_SEED);
+    }
     return random.nextBoolean();
   }
 
