7c078d8cfacd2872a1ab72186c3ede40116edeb4
David Paterson <dpaterson1@sheffield.ac.uk>
2018-07-04 17:07:04 +0100

    implementing remaining methods in GZoltarInstrumenter and adding result to test case based on deserialised info

--- client/src/main/java/org/kanonizo/instrumenters/GZoltarInstrumenter.java
+++ client/src/main/java/org/kanonizo/instrumenters/GZoltarInstrumenter.java
@@ -13,10 +13,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.kanonizo.Framework;
 import org.kanonizo.framework.ClassStore;
 import org.kanonizo.framework.TestCaseStore;
@@ -28,6 +31,8 @@ import org.kanonizo.framework.objects.LineStore;
 import org.kanonizo.framework.objects.SystemUnderTest;
 import org.kanonizo.framework.objects.TestCase;
 import org.kanonizo.framework.objects.TestSuite;
+import org.kanonizo.junit.KanonizoTestResult;
+import org.kanonizo.util.HashSetCollector;
 
 @org.kanonizo.annotations.Instrumenter
 public class GZoltarInstrumenter implements Instrumenter {
@@ -36,6 +41,7 @@ public class GZoltarInstrumenter implements Instrumenter {
   public static File gzFile = null;
 
   private HashMap<TestCase, Set<Line>> linesCovered = new HashMap<>();
+  private Set<Line> totalCoverage = new HashSet<>();
 
   @Override
   public Class<?> loadClass(String className) throws ClassNotFoundException {
@@ -47,65 +53,80 @@ public class GZoltarInstrumenter implements Instrumenter {
 
   }
 
+  private Spectrum spectrum = null;
+
+  private Spectrum getSpectrum() {
+    if (spectrum == null) {
+      final AgentConfigs agentConfigs = new AgentConfigs();
+      agentConfigs.setInstrumentationLevel(InstrumentationLevel.NONE);
+      try {
+        final SpectrumReader reader = new SpectrumReader(
+            Framework.getInstance().getSourceFolder().getAbsolutePath(), agentConfigs,
+            new FileInputStream(gzFile));
+        reader.read();
+        spectrum = reader.getSpectrum();
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    return spectrum;
+  }
+
   @Override
   public void collectCoverage() {
-    final AgentConfigs agentConfigs = new AgentConfigs();
-    agentConfigs.setInstrumentationLevel(InstrumentationLevel.NONE);
-    try {
-      final SpectrumReader reader = new SpectrumReader(Framework.getInstance().getSourceFolder().getAbsolutePath(), agentConfigs,
-            new FileInputStream(gzFile));
-      reader.read();
-      final Spectrum spectrum = reader.getSpectrum();
-      List<ProbeGroup> probeGroups = new ArrayList<>(spectrum.getProbeGroups()); // a ProbeGroup contains all probes (i.e., lines that have been instrumented) of each class
-      List<Transaction> transactions = spectrum.getTransactions(); // a transaction is in fact a test case execution
+    Spectrum spectrum = getSpectrum();
+    List<ProbeGroup> probeGroups = new ArrayList<>(spectrum
+        .getProbeGroups()); // a ProbeGroup contains all probes (i.e., lines that have been instrumented) of each class
+    List<Transaction> transactions = spectrum
+        .getTransactions(); // a transaction is in fact a test case execution
 
-      for (Transaction transaction : transactions) {
-        String tcName = transaction.getName();
-        String testClassName = tcName.substring(0, tcName.indexOf("#"));
-        String testMethodName = tcName.substring(tcName.indexOf("#")+1);
-        TestCase tc = TestCaseStore.with(testMethodName+"("+testClassName+")");
-        for (ProbeGroup probeGroup : probeGroups) {
-          for (Probe probe : probeGroup.getProbes()) {
-            if (transaction.isProbeActived(probeGroup, probe.getArrayIndex())) {
-              String className = probeGroup.getName();
-              ClassUnderTest cut = ClassStore.get(className);
-              int lineNumber = probe.getNode().getLineNumber();
-              Line l = LineStore.with(cut, lineNumber);
-              if(!linesCovered.containsKey(tc)){
-                linesCovered.put(tc, new HashSet<>());
-              }
-              linesCovered.get(tc).add(l);
-              // transaction covered this particular probe, i.e., line
+    for (Transaction transaction : transactions) {
+      String tcName = transaction.getName();
+      String testClassName = tcName.substring(0, tcName.indexOf("#"));
+      String testMethodName = tcName.substring(tcName.indexOf("#") + 1);
+      TestCase tc = TestCaseStore.with(testMethodName + "(" + testClassName + ")");
+      for (ProbeGroup probeGroup : probeGroups) {
+        for (Probe probe : probeGroup.getProbes()) {
+          if (transaction.isProbeActived(probeGroup, probe.getArrayIndex())) {
+            String className = probeGroup.getName();
+            ClassUnderTest cut = ClassStore.get(className);
+            int lineNumber = probe.getNode().getLineNumber();
+            Line l = LineStore.with(cut, lineNumber);
+            if (!linesCovered.containsKey(tc)) {
+              linesCovered.put(tc, new HashSet<>());
             }
+            linesCovered.get(tc).add(l);
+            totalCoverage.add(l);
+            // transaction covered this particular probe, i.e., line
           }
         }
-
-        if (transaction.hasFailed()) {
-          // transaction, i.e., test case failed
-        } else {
-          // transaction, i.e., test case passed
-        }
       }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch(Exception e){
-      e.printStackTrace();
+
+      tc.setResult(
+          new KanonizoTestResult(tc.getTestClass(), tc.getMethod(), transaction.hasFailed(),
+              Collections.emptyList(), transaction.getRuntime()));
     }
+
   }
 
   @Override
   public Set<Line> getLinesCovered(TestCase testCase) {
-    return null;
+    if (!linesCovered.containsKey(testCase)) {
+      return Collections.emptySet();
+    }
+    return linesCovered.get(testCase);
   }
 
   @Override
   public Set<Branch> getBranchesCovered(TestCase testCase) {
-    return null;
+    return Collections.emptySet();
   }
 
   @Override
   public int getTotalLines(ClassUnderTest cut) {
-    return 0;
+    return getLines(cut).size();
   }
 
   @Override
@@ -115,22 +136,34 @@ public class GZoltarInstrumenter implements Instrumenter {
 
   @Override
   public Set<Line> getLines(ClassUnderTest cut) {
-    return null;
+    Spectrum spectrum = getSpectrum();
+    List<ProbeGroup> probeGroups = new ArrayList<>(spectrum.getProbeGroups());
+    Optional<ProbeGroup> optGroup = probeGroups.stream()
+        .filter(pg -> pg.getName().equals(cut.getCUT().getName())).findFirst();
+    if (optGroup.isPresent()) {
+      ProbeGroup group = optGroup.get();
+      List<Probe> probes = optGroup.get().getProbes();
+      Set<Integer> lineNumbers = probes.stream().map(p -> p.getNode().getLineNumber())
+          .collect(Collectors
+              .toSet());
+      return lineNumbers.stream().map(l -> LineStore.with(cut, l)).collect(Collectors.toSet());
+    }
+    return Collections.emptySet();
   }
 
   @Override
   public Set<Branch> getBranches(ClassUnderTest cut) {
-    return null;
+    return Collections.emptySet();
   }
 
   @Override
   public int getTotalLines(SystemUnderTest sut) {
-    return 0;
+    return sut.getClassesUnderTest().stream().mapToInt(cut -> getTotalLines(cut)).sum();
   }
 
   @Override
   public int getLinesCovered(TestSuite testSuite) {
-    return 0;
+    return testSuite.getTestCases().stream().mapToInt(tc -> getLinesCovered(tc).size()).sum();
   }
 
   @Override
@@ -145,27 +178,29 @@ public class GZoltarInstrumenter implements Instrumenter {
 
   @Override
   public Set<Line> getLinesCovered(ClassUnderTest cut) {
-    return null;
+    return totalCoverage.stream().filter(l -> l.getParent().equals(cut))
+        .collect(Collectors.toSet());
   }
 
   @Override
   public Set<Line> getLinesCovered(SystemUnderTest sut) {
-    return null;
+    return sut.getClassesUnderTest().stream().map(cut -> getLinesCovered(cut))
+        .collect(new HashSetCollector<>());
   }
 
   @Override
   public Set<Branch> getBranchesCovered(ClassUnderTest cut) {
-    return null;
+    return Collections.emptySet();
   }
 
   @Override
   public Set<Branch> getBranchesCovered(SystemUnderTest sut) {
-    return null;
+    return Collections.emptySet();
   }
 
   @Override
   public ClassLoader getClassLoader() {
-    return null;
+    return Thread.currentThread().getContextClassLoader();
   }
 
   @Override
