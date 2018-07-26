2f343b4a4a00c6b313809f82e1e40672d67273bb
David Paterson <dpaterson1@sheffield.ac.uk>
2018-07-26 05:30:21 +0100

    ensuring jaccard distance is doing double division, and listening correctly to instrumenter property

--- client/src/main/java/org/kanonizo/framework/similarity/JaccardDistance.java
+++ client/src/main/java/org/kanonizo/framework/similarity/JaccardDistance.java
@@ -10,12 +10,18 @@ public class JaccardDistance implements DistanceFunction<TestCase> {
 
   private Instrumenter inst = Framework.getInstance().getInstrumenter();
 
+  public JaccardDistance() {
+    Framework.getInstance().addPropertyChangeListener(Framework.INSTRUMENTER_PROPERTY_NAME,
+        evt -> inst = (Instrumenter) evt.getNewValue()
+    );
+  }
+
   @Override
   public double getDistance(TestCase first, TestCase second) {
     Set<Line> covered1 = inst.getLinesCovered(first);
     Set<Line> covered2 = inst.getLinesCovered(second);
     //size of the intersection
-    int sim = covered1.stream().mapToInt(l -> covered2.contains(l) ? 1 : 0).sum();
+    double sim = covered1.stream().mapToDouble(l -> covered2.contains(l) ? 1 : 0).sum();
     //size of the union
     sim /= (covered1.size() + covered2.size() - sim);
     return sim;
